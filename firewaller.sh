5d0eb8269fc778b0267d6fc425052d004adf56b6
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2022-12-17 11:53:45 -0500

    Update firewaller.sh

--- firewaller.sh
+++ firewaller.sh
@@ -5,5 +5,7 @@ sudo ufw enable
 printf "\033[1;33mPlease edit the common-password file and the login.defs file.\n"
 sudo apt install firewalld
 systemctl start firewalld && systemctl enable firewalld
+printf "\033[1;33m"
 grep -E "PASS_MIN_DAYS|PASS_MAX_DAYS" /etc/login.defs
+printf "\033[0m"
 sudo nano /etc/login.defs
